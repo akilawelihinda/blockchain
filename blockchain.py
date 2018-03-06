156065b7a495d69ff5a4aa82978a011080d8ea55
Akila Welihinda <akilawelihinda@ucla.edu>
2018-03-06 00:01:38 -0800

    added consensus algorithm

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,11 @@
 import hashlib
 import json
+import sys
 from time import time
 from uuid import uuid4
 from textwrap import dedent
+from urllib.parse import urlparse
+import requests
 
 from flask import Flask, jsonify, request
 
@@ -11,6 +14,11 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
         self.new_block(proof=369, previous_hash=0xdeadbeef)
+        self.nodes = set()
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     # Creates a new block and adds it to the chain
     # Only specify previous hash if creating genesis block
@@ -58,7 +66,38 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+    def valid_chain(self, chain):
+        current_index = 1
+        while(current_index < len(chain)):
+            current_block = chain[current_index]
+            previous_block = chain[current_index-1]
+            if not self.valid_proof(previous_block['proof'], current_block['proof']):
+                return False
+            if (self.hash(previous_block) != current_block['previous_hash']):
+                return False
+            current_index += 1
+        return True
 
+    # Consensus algorithm: Pick longest valid chain from neighboring nodes
+    def resolve_conflicts(self):
+        neighbors = self.nodes
+        new_chain = None
+        max_len = len(self.chain)
+
+        for neighbor in neighbors:
+             response = requests.get(f'http://{neighbor}/chain')
+             if response.status_code == 200:
+                 length = response.json()['length']
+                 chain = response.json()['chain']
+                 if length > max_len and self.valid_chain(chain):
+                     new_chain = chain
+                     max_len = length
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
 # Flask logic
 app = Flask(__name__)
@@ -87,11 +126,11 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = request.args
+    values = request.get_json(force=True)
     required = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required):
         return 'Missing values', 400
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    index = blockchain.new_transaction(values.get('sender'), values.get('recipient'), values.get('amount'))
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
 
@@ -103,5 +142,42 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json(force=True)
+    nodes = values.get("nodes")
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'This node\'s chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'This node\'s chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if(len(sys.argv) < 2):
+        sys.exit("Invalid number of arguements")
+    chosen_port = int(sys.argv[1])
+    app.run(host='127.0.0.1', port=chosen_port)
